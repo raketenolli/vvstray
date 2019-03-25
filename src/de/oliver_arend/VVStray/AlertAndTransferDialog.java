c352519c669a34bee1f670948027d4b386b5514d
oarend <oliver.arend@itk-engineering.de>
2019-03-25 10:03:53 +0100

    improve formatting of traffic alerts and transfers

--- src/de/oliver_arend/VVStray/AlertAndTransferDialog.java
+++ src/de/oliver_arend/VVStray/AlertAndTransferDialog.java
@@ -1,5 +1,6 @@
 package de.oliver_arend.VVStray;
 
+import java.awt.FlowLayout;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.util.ArrayList;
@@ -8,6 +9,9 @@ import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 
 public class AlertAndTransferDialog extends JDialog {
 	
@@ -22,9 +26,19 @@ public class AlertAndTransferDialog extends JDialog {
 		}
 		this.setIconImage(new ImageIcon("resources/vvslogo_16x16.png").getImage());
 		this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
-		alerts.forEach((alert) -> this.add(new JLabel(alert)));
-		this.add(new JLabel(" "));
-		transfers.forEach((transfer) -> this.add(new JLabel(transfer)));
+		alerts.forEach((alert) -> {
+			JPanel alertPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+			alertPanel.add(new JLabel(alert, new ImageIcon("resources/alert.png"), SwingConstants.LEFT));
+			this.add(alertPanel);
+		});
+		if(alerts.size() > 0 && transfers.size() > 0) {
+			this.add(new JSeparator(SwingConstants.HORIZONTAL));
+		}
+		transfers.forEach((transfer) -> {
+			JPanel transferPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+			transferPanel.add(new JLabel(transfer, new ImageIcon("resources/transfer.png"), SwingConstants.LEFT));
+			this.add(transferPanel);
+		});
 		this.pack();
 		Point mousePosition = MouseInfo.getPointerInfo().getLocation();
         this.setLocation(mousePosition.x - this.getWidth(), mousePosition.y - this.getHeight() - 32);
